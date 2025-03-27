7ce3126064396cd024486f19eb3c815d1e7d7dfc
boii443 <tdashinobi@gmail.com>
2025-03-26 20:32:51 -0400

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY SUBZERO🤍*",
 // set the auto reply massage on status reply  
-PREFIX: process.env.PREFIX || "@",
+PREFIX: process.env.PREFIX || "*",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "᭄࿐𝐓𝐃𝐀𒋲𝐒𝚮𝚰𝚴𝚯𝚩𝚰√",
 // add bot namw here for menu
